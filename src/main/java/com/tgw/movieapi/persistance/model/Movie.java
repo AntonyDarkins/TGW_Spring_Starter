a0dd921d1ace666d2ad9cb6105712558beb59832
chrisYiangou <chrisyiangou100@gmail.com>
2022-09-07 15:59:25 +0100

    service methods made

--- src/main/java/com/tgw/movieapi/persistance/model/Movie.java
+++ src/main/java/com/tgw/movieapi/persistance/model/Movie.java
@@ -10,9 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.tgw.movieapi.persistance.repo.MovieRepo;
 
 @Entity
 public class Movie {
@@ -23,6 +22,7 @@ public class Movie {
 	private Integer id;
 	
 	@Size(min = 2, max = 100)
+//	@NotNull
 	private String title;
 	
 	@Min(0)
--- src/main/java/com/tgw/movieapi/service/MovieService.java
+++ src/main/java/com/tgw/movieapi/service/MovieService.java
@@ -1,5 +1,7 @@
 package com.tgw.movieapi.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +25,36 @@ public class MovieService {
 	} 
 	
 	//CRUD -- Create One and Create Many, Read, Update and my Delete.
-
+	
+	public Movie readyById(Integer id) {
+		return this.repo.findById(id).orElse(null);
+	}
+	
+	public Movie update(Movie updatedMovie, Integer id) {
+		
+		Optional<Movie> moviePresent = this.repo.findById(id);
+		
+		if (moviePresent.isPresent() && moviePresent.get().getId() == updatedMovie.getId()) {
+			Movie oldMovie = moviePresent.get();
+			oldMovie.setTitle(updatedMovie.getTitle());
+			oldMovie.setRating(updatedMovie.getRating());
+			oldMovie.setReleaseDate(updatedMovie.getReleaseDate());
+			this.repo.saveAndFlush(oldMovie);
+			return oldMovie;		
+		}
+		return null;
+	}
+	
+	public Boolean delete(Integer id) {
+		Optional<Movie> moviePresent = this.repo.findById(id);
+		
+		if (moviePresent.isPresent()) {
+			this.repo.deleteById(id);
+			return true;
+		}
+		
+		return false;
+	}
+	
+ 
 }
